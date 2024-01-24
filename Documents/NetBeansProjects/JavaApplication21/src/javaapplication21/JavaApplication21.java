2ff06cf072afd17ba0f68830ffd46a353e0fd41d
alexale980 <alek@sheridancollege.ca>
2024-01-24 10:17:32 -0500

    version 2

--- Documents/NetBeansProjects/JavaApplication21/src/javaapplication21/JavaApplication21.java
+++ Documents/NetBeansProjects/JavaApplication21/src/javaapplication21/JavaApplication21.java
@@ -14,7 +14,16 @@ public class JavaApplication21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
+        // Declare two numbers
+        int num1 = 5;
+        int num2 = 10;
+      
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
     
 }
